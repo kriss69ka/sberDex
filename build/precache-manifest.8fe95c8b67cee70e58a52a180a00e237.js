c09a55ece0df1f8ef3c1d73a670e16e5d40f45ec
KristinaKim <kriss_69ka@gmail.com>
2019-10-27 15:01:36 +0300

    build

---
+++ build/precache-manifest.8fe95c8b67cee70e58a52a180a00e237.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6699ca29a9cb08edb3ca2ab6cf1f3678",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ff5ae628dcb415cae97",
+    "url": "/static/css/main.5918d66e.chunk.css"
+  },
+  {
+    "revision": "4252625e9ccbaa0f01b2",
+    "url": "/static/js/2.89ab31df.chunk.js"
+  },
+  {
+    "revision": "1ff5ae628dcb415cae97",
+    "url": "/static/js/main.442e9e4f.chunk.js"
+  },
+  {
+    "revision": "5ef2c9768c4093f8fa6a",
+    "url": "/static/js/runtime-main.8e55a701.js"
+  },
+  {
+    "revision": "97a3de791a179274ba44febddf4ce3f8",
+    "url": "/static/media/pokeball.97a3de79.png"
+  }
+]);
